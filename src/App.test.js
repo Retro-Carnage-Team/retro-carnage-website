3eae2bf167b2469e6bde73433c6f6ab1e59c8193
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2020-03-29 12:58:50 +0200

    Azure CI test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders start screen with game title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/DOGS OF WAR II/i);
   expect(linkElement).toBeInTheDocument();
 });
