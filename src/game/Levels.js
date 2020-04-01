bbd7d6be786f26c13e36086232444469fc6c81ce
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2020-04-01 17:20:14 +0200

    Worked on mission meta data

--- src/game/Levels.js
+++ src/game/Levels.js
@@ -1,5 +1,5 @@
 const Levels = [{
-    briefing: "I am Berlin's hottest influencer. 30.000 followers at Insta alone - and that's not even counting Youtube and Twitter. My biggest competitor is envious and spreads the rumour that I am not vegan. Take out my competitor - for $10.000.",
+    briefing: "I am Berlin's hottest influencer. 30.000 followers on Insta alone - and that's not even counting Youtube and Twitter. My biggest competitor is envious and spreads the rumour that I am not vegan. Take out my competitor - for $10.000.",
     client: "images/tiles/clients/angry-beard-blur-close-up.jpg",
     location: {        
         latitude: 165,
@@ -7,6 +7,105 @@ const Levels = [{
     },
     name: "Berlin",
     reward: 10000
+}, {
+    briefing: "My name is Dr. Valentin Pillmann. I have spent most of my life in the ... Zone ... and risked a lot to get hold of a valuable artifact called Nightstar. Bandits robbed me and stole it from me. Get the artifact back and you'll get $8,000.",
+    client: "images/tiles/clients/adult-aged-black-and-white-close-up.jpg",
+    location: {        
+        latitude: 158,
+        longitude: 713
+    },
+    name: "Minsk",
+    reward: 8000
+}, {
+    briefing: "",
+    client: "images/tiles/clients/woman-wearing-black-long-sleeved-shirt.jpg",
+    location: {        
+        latitude: 213,
+        longitude: 734
+    },
+    name: "Istanbul",
+    reward: 5000
+}, {
+    briefing: "The leader of a local rebel organization bought missiles from me to enforce his legitimate interests. During the handover, special forces of an international military alliance appeared and confiscated the weapons. Return the missiles to my client to restore my reputation. I'll pay you $12,000.",
+    client: "images/tiles/clients/bald-man-with-a-serious-facial-expression.jpg",
+    location: {        
+        latitude: 249,
+        longitude: 684
+    },
+    name: "Tripolis",
+    reward: 12000
+}, {
+    briefing: "",
+    client: "images/tiles/clients/close-up-photo-of-man-s-face.jpg",
+    location: {        
+        latitude: 384,
+        longitude: 800
+    },
+    name: "Mogadischu",
+    reward: 5000
+}, {
+    briefing: "",
+    client: "images/tiles/clients/man-sitting-near-purple-wall.jpg",
+    location: {        
+        latitude: 311,
+        longitude: 895
+    },
+    name: "Mumbai",
+    reward: 2000
+}, {
+    briefing: "",
+    client: "images/tiles/clients/man-in-brown-coat-and-gray-button-up-shirt.jpg",
+    location: {        
+        latitude: 199,
+        longitude: 871
+    },
+    name: "Bischkek",
+    reward: 5000
+}, {
+    briefing: "I am a preacher for a christian community in Colorado. We help our fellow believers, the Karen people of Myanmar, with Bibles and medicine. Unfortunately, we also need your help to protect the Karen against the local military. My community will pay you $10,000 for your services.",
+    client: "images/tiles/clients/man-in-black-shirt.jpg",
+    location: {        
+        latitude: 312,
+        longitude: 973
+    },
+    name: "Burma",
+    reward: 10000
+}, {
+    briefing: "",
+    client: "images/tiles/clients/adult-aged-beard-elder.jpg",
+    location: {        
+        latitude: 338,
+        longitude: 1018
+    },
+    name: "Ho-Chi-Minh City",
+    reward: 10000
+}, {
+    briefing: "",
+    client: "images/tiles/clients/photo-of-girl-wearing-brown-shirt.jpg",
+    location: {        
+        latitude: 299,
+        longitude: 295
+    },
+    name: "Mexico City",
+    reward: 1000
+}, {
+    briefing: "",
+    client: "images/tiles/clients/man-wearing-blue-crew-neck-t-shirt.jpg",
+    location: {        
+        latitude: 257,
+        longitude: 309
+    },
+    name: "San Antonio",
+    reward: 10000
+}, {
+    briefing: "I was once a successful .com entrepreneur and retired to the Amazon jungle many years ago I. Actually everything is really cool here. But the last few months, another hermit named Bruce Banner has been bugging me. Solve this problem for me and I'll pay you $10,000.",
+    client: "images/tiles/clients/man-wearing-black-sunglasses-and-black-beanie.jpg",
+    location: {        
+        latitude: 398,
+        longitude: 412
+    },
+    name: "Amazonas",
+    reward: 5000
 }];
 
 export default Levels;
