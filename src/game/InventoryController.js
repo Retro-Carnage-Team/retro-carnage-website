06b4c3a82e9b39e35abdb751f710a52c9d6f4aee
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2020-04-04 12:57:10 +0200

    Added InventoryController skeleton

---
+++ src/game/InventoryController.js
@@ -0,0 +1,16 @@
+import Ammunition from './Ammunition';
+import Grenades from './Grenades';
+import Weapons from './Weapons';
+
+class InventoryController {
+
+    constructor() {
+        this.cash = 0;
+    }
+
+    
+
+}
+
+const inventoryControllerInstance = new InventoryController();
+export default inventoryControllerInstance;
---
+++ src/game/InventoryController.test.js
@@ -0,0 +1,5 @@
+import InventoryController from './InventoryController';
+import Ammunition from './Ammunition';
+import Grenades from './Grenades';
+import Weapons from './Weapons';
+
