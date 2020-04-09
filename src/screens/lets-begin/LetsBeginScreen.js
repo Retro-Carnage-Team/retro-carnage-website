a3f52bd4dbf06cc1b49e85aa595615f3e2f8de7f
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2020-04-09 22:15:06 +0200

    Added missing !

--- src/screens/lets-begin/LetsBeginScreen.js
+++ src/screens/lets-begin/LetsBeginScreen.js
@@ -18,7 +18,7 @@ function LetsBeginMessageScreen(props) {
   return (
     <div className="lets-begin-screen">
       <div style={{ height: height + "px" }} />
-      <h1>Let the mission begin</h1>
+      <h1>Let the mission begin!</h1>
     </div>
   );
 }
