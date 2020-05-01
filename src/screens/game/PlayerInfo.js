06dd63e597856a4910b7f72a6febd00d58451e87
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2020-05-01 20:59:33 +0200

    Worked on static areas left and right of the game screen

--- src/screens/game/PlayerInfo.js
+++ src/screens/game/PlayerInfo.js
@@ -9,21 +9,29 @@ export default class PlayerInfo extends React.Component {
 
   render() {
     return (
-      <div className={`player-info-${this.props.player}`}>
+      <div className="player-info">
         <img 
           alt={`Portrait of player ${this.props.player}`} 
-          id="player-portrait" 
+          className="player-portrait" 
           src={`images/backgrounds/portrait-player-${this.props.player}.jpg`} />
         
-        <h2 className="score">3100</h2>
+        <div className="score-container">
+          <h2>3100</h2>
+        </div>
+
         <div className="weapon-container">
-          
+          <img 
+            alt={`Selected weapon of player ${this.props.player}`} 
+            className="selected-weapon" 
+            src="images/tiles/weapons/G36-r.png" />
+          <h2>2154</h2>
         </div>
-        <div className="ammunition-container">
-          <span>2154</span>
-        </div>
+
         <div className="lives-container">
-          <span>5</span>
+          <img src="images/backgrounds/life-player-1.png" alt="" />
+          <img src="images/backgrounds/life-player-1.png" alt="" />
+          <img src="images/backgrounds/life-player-1.png" alt="" />
+          <img src="images/backgrounds/life-player-1.png" alt="" />
         </div>
       </div>
     );
