00cb32f7f5e1be5fe99c750941b542e3b4a7cab5
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2020-05-02 18:40:57 +0200

    Fixed minor issues

--- src/game/ChangeListener.js
+++ src/game/ChangeListener.js
@@ -13,4 +13,4 @@ export default class ChangeListener {
     this.callback(newValue, changedProperty);
   }
 
-};
+}
--- src/game/ChangeListener.test.js
+++ src/game/ChangeListener.test.js
@@ -8,13 +8,13 @@ test('ChangeListener should handle matching property', () => {
 
 test('ChangeListener should not handle other properties', () => {
   const propertyName = 'name';
-  const listener = new ChangeListener((value) => { console.log(value); }, propertyName);
+  const listener = new ChangeListener(() => { }, propertyName);
   expect(listener.handlesProperty(`not-${propertyName}`)).toBeFalsy();
 });
 
 test('ChangeListener should handle all properties when not specified', () => {
   const propertyName = 'name';
-  const listener = new ChangeListener((value) => { console.log(value); });
+  const listener = new ChangeListener(() => { });
   expect(listener.handlesProperty(propertyName)).toBeTruthy();
   expect(listener.handlesProperty(`not-${propertyName}`)).toBeTruthy();
 });
@@ -28,7 +28,7 @@ test('ChangeListener should call the specified callback correctly', () => {
     called = true;
     expect(value).toBe(newValue);
     expect(propertyName).toBe(propertyNameValue);
-  }
+  };
   
   const filteredListener = new ChangeListener(callback, propertyNameValue);
   filteredListener.call(newValue, propertyNameValue);
