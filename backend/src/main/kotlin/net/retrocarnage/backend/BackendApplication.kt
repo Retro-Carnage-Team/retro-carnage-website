aa010a0f1718ffab90b8cef636a8db542c5e3a94
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2020-08-23 22:16:07 +0200

    Added missing annotation to enable scheduling

--- backend/src/main/kotlin/net/retrocarnage/backend/BackendApplication.kt
+++ backend/src/main/kotlin/net/retrocarnage/backend/BackendApplication.kt
@@ -2,8 +2,10 @@ package net.retrocarnage.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class BackendApplication
 
 fun main(args: Array<String>) {
