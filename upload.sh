4839460a3d43b7baef8f28d9cd5794e5b3e10ca5
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2024-10-03 10:57:04 +0200

    fix-upload-script

--- upload.sh
+++ upload.sh
@@ -4,7 +4,7 @@ LCD="./site"
 RCD="/"
 
 lftp -f "
-open ${{env.$FTP_HOST}}
+open ${{env.FTP_HOST}}
 user ${{secrets.FTP_USER}} ${{secrets.FTP_PASS}}
 mirror --continue --reverse --verbose $LCD $RCD
 bye
