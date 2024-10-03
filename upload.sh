2b37155a7733bf0c810f1e38ec1fd3045497fed5
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2024-10-03 11:02:20 +0200

    passing env variables to upload script

--- upload.sh
+++ upload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-FTP_HOST=${{env.FTP_HOST}}
+FTP_HOST=$1
+FTP_USER=$2
+FTP_PASS=$3
 LCD="./site"
 RCD="/"
 
@@ -8,7 +10,7 @@ echo $FTP_HOST
 
 lftp -f "
 open $FTP_HOST
-user ${{secrets.FTP_USER}} ${{secrets.FTP_PASS}}
+user $FTP_USER $FTP_PASS
 mirror --continue --reverse --verbose $LCD $RCD
 bye
 " 
